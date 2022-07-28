1997485a7ddbd0e0279e9af0179c2c9e2f39a624
aiin'tdeadyet <jamesdcritchlow@gmail.com>
2022-07-27 22:16:50 -0600

    board finished being drawn

--- main.py
+++ main.py
@@ -12,9 +12,56 @@ IMAGES = {}
 
 def load_images():
     '''loads images from the pieces folder'''
-    pieces = ['bb', 'bn', 'bq', 'bk', 'bp', 'wb', 'wn', 'wq', 'wk', 'wp'] # names of files to load
+    pieces = ['br', 'bb', 'bn', 'bq', 'bk', 'bp', 'wb', 'wn', 'wq', 'wk', 'wp', 'wr'] # names of files to load
     for piece in pieces:
         IMAGES[piece] = pygame.transform.scale(pygame.image.load("pieces/" + str(piece) + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+def draw_game(screen, game):
+    '''draws the board and peaces'''
+    draw_board(screen) # helper function
+    draw_pieces(screen, game) # helper function
+
+# helper functions for draw_game
+def draw_board(screen):
+    '''draws the board'''
+    colors = [pygame.Color('white'), pygame.Color('gray')]
+    for i in range(DIMENSIONS):
+        for j in range(DIMENSIONS):
+            color = colors[((i+j) % 2)]
+            pygame.draw.rect(screen, color, pygame.Rect(j*SQ_SIZE, i*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def draw_pieces(screen, game):
+    '''draws the pieces'''
+    for i in range(DIMENSIONS):
+        for j in range(DIMENSIONS):
+            match game.board[i][j]:
+                case 'b':
+                    screen.blit(IMAGES['bb'], (j*SQ_SIZE, i*SQ_SIZE))
+                case 'n':
+                    screen.blit(IMAGES['bn'], (j*SQ_SIZE, i*SQ_SIZE))
+                case 'q':
+                    screen.blit(IMAGES['bq'], (j*SQ_SIZE, i*SQ_SIZE))
+                case 'k':
+                    screen.blit(IMAGES['bk'], (j*SQ_SIZE, i*SQ_SIZE))
+                case 'p':
+                    screen.blit(IMAGES['bp'], (j*SQ_SIZE, i*SQ_SIZE))
+                case 'r':
+                    screen.blit(IMAGES['br'], (j*SQ_SIZE, i*SQ_SIZE))
+                case 'B':
+                    screen.blit(IMAGES['wb'], (j*SQ_SIZE, i*SQ_SIZE))
+                case 'N':
+                    screen.blit(IMAGES['wn'], (j*SQ_SIZE, i*SQ_SIZE))
+                case 'Q':
+                    screen.blit(IMAGES['wq'], (j*SQ_SIZE, i*SQ_SIZE))
+                case 'K':
+                    screen.blit(IMAGES['wk'], (j*SQ_SIZE, i*SQ_SIZE))
+                case 'P':
+                    screen.blit(IMAGES['wp'], (j*SQ_SIZE, i*SQ_SIZE))
+                case 'R':
+                    screen.blit(IMAGES['wr'], (j*SQ_SIZE, i*SQ_SIZE))
+                case ' ':
+                    pass
 
 
 def main():
@@ -23,7 +70,6 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
-    screen.fill(pygame.Color('white'))
     game_state = engine.game()
     load_images() 
     running = True
@@ -33,18 +79,12 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                # get the mouse position
-                pos = pygame.mouse.get_pos()
-                # get the square that the mouse is on
-                row, col = pos[1] // SQ_SIZE, pos[0] // SQ_SIZE
-                # get the piece that is on that square
-                piece = game_state.board[row][col]
-                # if the piece is a white piece and it is the player's turn, move the piece
-                if piece.isupper() and game_state.turn == 'white':
-                    game_state.move(row, col)
-                # if the piece is a black piece and it is the player's turn, move the piece
-                elif piece.islower() and game_state.turn == 'black':
-                    game_state.move(row, col)
+                pass
+            
+            draw_game(screen, game_state)
+            clock.tick(MAX_FPS)
+            pygame.display.flip()
+
 
 
 if __name__ == "__main__":
